8bec7848485de68b9ea78526f08338f92bad285e
D2D4 <18723798+Dimitreee@users.noreply.github.com>
2023-03-28 22:13:50 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/unfreezer/",
+  base: "/",
   plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000,
